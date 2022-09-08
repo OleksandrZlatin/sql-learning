fb9dddcc70d5ae1d7ba51c766c0a04fec063db69
OleksandrZlatin <110457381+OleksandrZlatin@users.noreply.github.com>
2022-09-08 10:04:28 +0300

    Delete 15% or -15% if promo_price is bigger.sql

--- 15% or -15% if promo_price is bigger.sql
+++
@@ -1,4 +0,0 @@
-SELECT category_id, name, price, promo_price, -- discount (percent from price) offered by the promo_price. for example, 15% or -15% if promo_price is bigger
-ROUND((price-promo_price)/price*100) AS 'Discount %'
-FROM Products
-WHERE promo_price<Price;
